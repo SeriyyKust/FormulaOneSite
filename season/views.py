8cc8bec7209866a1d504c6511faf0e0af901e420
SeriyyKust <tima.zavrazhnov@mail.ru>
2022-04-01 16:30:49 +0300

    Create base Templates

--- season/views.py
+++ season/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
 
 def base_page(request):
-    return HttpResponse("Main Page!")
+    drivers = Drivers.objects.all()
+    return render(request, 'season/base_page.html', {'title': 'Season 2022', 'menu': menu, 'drivers': drivers})
 
 
 def teams(request, tid):
